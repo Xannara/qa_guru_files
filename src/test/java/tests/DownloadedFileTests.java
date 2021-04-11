f6cbdf591e4e38d6e802149e6567fe4e3c1a1391
Xannara <roksanasky@gmail.com>
2021-04-12 01:39:15 +0300

    added download file tests

---
+++ src/test/java/tests/DownloadedFileTests.java
@@ -0,0 +1,28 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import utils.Files;
+
+import java.io.File;
+import java.io.IOException;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
+public class DownloadedFileTests {
+
+    @Test
+    void selenideDownloadReadmeTest() throws IOException {
+        Configuration.downloadsFolder = "./downloads";
+        String expectedText = "Selenide = UI Testing Framework powered by Selenium WebDriver";
+
+        open("https://github.com/selenide/selenide/blob/master/README.md");
+        File downloadedFile = $("#raw-url").download();
+        String fileContent = Files.readTextFromFile(downloadedFile);
+
+        assertThat(fileContent, containsString(expectedText));
+    }
+}
